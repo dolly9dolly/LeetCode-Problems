978b43c7ded5062027456f8cc016a481ebc7094e
Dolly <138841447+dolly9dolly@users.noreply.github.com>
2026-06-03 23:06:21 -0500

    Time: 38 ms (36.86%), Space: 47.3 MB (23.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,36 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+        int n = nums.length;
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if((nums[i] + nums[j]) == target) {
+                    return new int[]{i,j};
+                }
             }
-
-            map.put(nums[i], i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
-
-//for sorted array using 2 pointers.
-        // int left=0;
-        // int right = n-1;
-        // while(left<right)
-        // { 
-        //     if((nums[left]+nums[right])<target)
-        //     {
-        //        left++;
-        //     }
-        //     else if((nums[left]+nums[right])>target)
-        //     {
-        //        right--;
-        //     }
-        //      else if((nums[left]+nums[right])==target)
-        //     {
-        //        return new int[]{left,right};
-        //     }
-        // }
-        // return new int[0];
