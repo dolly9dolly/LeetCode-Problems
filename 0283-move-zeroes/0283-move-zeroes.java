4719679223c1c06d9fc086ae376c483b621e7359
Dolly <138841447+dolly9dolly@users.noreply.github.com>
2026-06-07 16:27:05 -0500

    Time: 2 ms (91.74%), Space: 48 MB (19.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,35 +1,15 @@
-
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int i=0,j=i+1;
-        int temp;
-        while(j<n)
-        {
-            if(nums[i]!=0 && nums[j]==0)
-            {
-                i=j;
-                j++;
+        int slow = 0;
+        for(int fast=0;fast<nums.length;fast++){
+            if(nums[fast] != 0){
+                nums[slow] = nums[fast];
+                slow++;
             }
-            else if(nums[i]==0 && nums[j]==0)
-            {
-                j++;
-            }
-            else if(nums[i]!=0 && nums[j]!=0)
-            {
-                i++;
-                j++;
-            }
-            else if(nums[i]==0 && nums[j]!=0)
-            {
-                temp = nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
-                i++;
-                j++;
-            }
-
         }
-       
+        for(int i=slow;i<nums.length;i++){
+            nums[i] = 0;
+        }
+        
     }
 }
